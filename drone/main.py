8280d2c3acdb68e758b375e21fca6292e0bbd358
RanaRehman2009 <abdulrreh2009@gmail.com>
2025-07-18 14:38:05 -0500

    Backend

--- drone/main.py
+++ drone/main.py
@@ -1,23 +1,47 @@
 from pymavlink import mavutil
-import time
+import time, serial
+
+# Cellular Module Settings
+
+CM_PORT = '/dev/ttyUSB2'
+CM_BAUD = 115200
+
+# Drone Settings
+DRONE_PORT = '/dev/ttyUSB1'
+
+
 
 # Establish connection
-connect_to_drone = mavutil.mavlink_connection('COM4', baud=57600)
+master = mavutil.mavlink_connection(DRONE_PORT) # meant for raspberry pi
+# connect_to_drone = mavutil.mavlink_connection('COM4', baud=57600) # meant for windows
 
 print("Waiting for heartbeat...")
-
-# Wait for the first heartbeat 
-connect_to_drone.wait_heartbeat()
+master.wait_heartbeat()
 print("Heartbeat received")
 print("Heartbeat from system (system %u component %u)" % 
-      (connect_to_drone.target_system, connect_to_drone.target_component))
+      (master.target_system, master.target_component))
 
-# Listen for GPS messages only
-while True:
-    msg = connect_to_drone.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
-    if msg:
-        lat = msg.lat / 1e7   # Convert to decimal degrees
-        lon = msg.lon / 1e7
-        alt = msg.alt / 1000  # Convert from millimeters to meters
-        print(f"Latitude: {lat}, Longitude: {lon}, Altitude: {alt} m")
-    time.sleep(0.1)
+def connect_to_cellular():
+      ser = serial.Serial(CM_PORT, CM_BAUD, timeout=1)
+      if not ser.is_open:
+            ser.open()
+      print("Connected to cellular module")
+      return ser
+
+def send_at_command(ser, command, response_timeout=2):
+      ser.write(command + '\r'.encode())
+      time.sleep(response_timeout)
+      response = ser.read_all().decode(errors='ignore')
+      return response.strip()
+
+def main():
+      ser = connect_to_cellular()
+      print("RSSI & BER: ", send_at_command(ser, "AT+CSQ"))
+      print("RSRP, RSRQ, SINR", send_at_command(ser, "AT+CESQ"))
+      print(" Serving Cell Info (Includes RSRP, RSRQ, SINR, Cell ID, PCI)", send_at_command(ser, "AT+SIMCOMATI"))
+      ser.close()
+
+if __name__ == "__main__":
+      main()
+
+
